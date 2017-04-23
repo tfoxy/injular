cd90b8a7d72ac7c92060fd5a8f1ee62e27f16762
Tomás Fox <tomas.c.fox@gmail.com>
2017-04-22 22:17:35 -0300

    Add attachToModule function

--- src/injular.js
+++ src/injular.js
@@ -162,7 +162,43 @@ function injectComponent(name, options, injularData) {
 }
 
 
+function injularCompile($compileNode, templateAttrs, childTranscludeFn) {
+  const node = $compileNode[0];
+  node.$injularTemplate = node.outerHTML;
+  // eslint-disable-next-line no-underscore-dangle, max-len
+  return this._nonInjularCompile && this._nonInjularCompile($compileNode, templateAttrs, childTranscludeFn);
+}
+
+
+function injularDirective(name, directiveFactory) {
+  // eslint-disable-next-line no-underscore-dangle
+  return this._nonInjularDirective(name, ['$injector', ($injector) => {
+    const directive = instantiateDirective(name, directiveFactory, $injector);
+    if (!directive.template && directive.restrict === 'E') {
+      // eslint-disable-next-line no-underscore-dangle,
+      directive._nonInjularCompile = directive.compile;
+      directive.compile = injularCompile;
+    }
+    return directive;
+  }]);
+}
+
+
+function attachToModule(module, injularData) {
+  /* eslint-disable no-param-reassign, no-underscore-dangle */
+  module.config(['$compileProvider', ($compileProvider) => {
+    if ('_nonInjularDirective' in $compileProvider) return;
+    $compileProvider._nonInjularDirective = $compileProvider.directive;
+    $compileProvider.directive = injularDirective;
+  }]).run(['$injector', ($injector) => {
+    injularData.$injector = $injector;
+  }]);
+  /* eslint-enable no-param-reassign, no-underscore-dangle */
+}
+
+
 const injular = {
+  attachToModule,
   injectComponent,
   injectDirective,
 };
--- test/injular.spec.js
+++ test/injular.spec.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import injular from '../src/injular';
 
 const MODULE_NAME = 'testModule';
+const INJULAR_MODULE_NAME = 'injularModule';
 
 describe('injular', () => {
   describe('.injectComponent', () => {
@@ -18,18 +19,18 @@ describe('injular', () => {
         template: '{{$ctrl.msg}}',
       });
 
-      const element = document.createElement('div');
-      element.innerHTML = '<test-component></test-component>';
-      angular.bootstrap(element, [MODULE_NAME]);
+      const rootElement = document.createElement('div');
+      rootElement.innerHTML = '<test-component></test-component>';
+      angular.bootstrap(rootElement, [MODULE_NAME]);
 
-      expect(element.textContent).to.equal('foo');
+      expect(rootElement.textContent).to.equal('foo');
 
       injular.injectComponent('testComponent', {
         controller: function TestComponent() { this.msg = 'bar'; },
         template: '{{$ctrl.msg}}',
       }, injularData);
 
-      expect(element.textContent).to.equal('bar');
+      expect(rootElement.textContent).to.equal('bar');
     });
   });
 
@@ -53,11 +54,11 @@ describe('injular', () => {
         },
       }));
 
-      const element = document.createElement('div');
-      element.innerHTML = '<test-component>{{$ctrl.msg}}</test-component>';
-      angular.bootstrap(element, [MODULE_NAME]);
+      const rootElement = document.createElement('div');
+      rootElement.innerHTML = '<test-component>{{$ctrl.msg}}</test-component>';
+      angular.bootstrap(rootElement, [MODULE_NAME]);
 
-      expect(element.textContent).to.equal('foo');
+      expect(rootElement.textContent).to.equal('foo');
 
       injular.injectDirective('testComponent', () => ({
         bindToController: {},
@@ -71,7 +72,44 @@ describe('injular', () => {
         },
       }), injularData);
 
-      expect(element.textContent).to.equal('bar');
+      expect(rootElement.textContent).to.equal('bar');
+    });
+  });
+
+  describe('.attachToModule', () => {
+    it('should add $injector to injularData after bootstrapping module', () => {
+      const injularData = {};
+
+      const module = angular.module(INJULAR_MODULE_NAME, []);
+      injular.attachToModule(module, injularData);
+
+      const rootElement = document.createElement('div');
+      angular.bootstrap(rootElement, [INJULAR_MODULE_NAME]);
+
+      expect(injularData).to.have.property('$injector').to.be.an('object');
+    });
+
+    it('should patch directive registration so that $injularTemplate is set to the element when no template is provided', () => {
+      const injularData = {};
+
+      const injularModule = angular.module(INJULAR_MODULE_NAME, []);
+      injular.attachToModule(injularModule, injularData);
+
+      angular.module(MODULE_NAME, [])
+      .directive('testComponent', () => ({
+        bindToController: {},
+        controller: function TestComponent() { this.msg = 'foo'; },
+        controllerAs: '$ctrl',
+        restrict: 'E',
+        scope: true,
+      }));
+
+      const rootElement = document.createElement('div');
+      const template = '<test-component>{{$ctrl.msg}}</test-component>';
+      rootElement.innerHTML = template;
+      angular.bootstrap(rootElement, [INJULAR_MODULE_NAME, MODULE_NAME]);
+
+      expect(rootElement.firstChild).to.have.property('$injularTemplate', template);
     });
   });
 });
